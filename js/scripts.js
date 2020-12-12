911b954e3c4b42bd245f6720cdaeea47ef122225
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2020-12-12 22:34:17 +0500

    add html and js

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+var elForm = document.querySelector('.js-form');
+var elInput = elForm.querySelector('.js-input');
+var elBtn = elForm.querySelector('.js-btn');
+var elOutput =elForm.querySelector('.js-warning-output');
+var compNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+var elUrinishlarSoni = elForm.querySelector('.js-urinishlar-soni');
+var elUrinishlarOutputArray = elForm.querySelector('.js-urinishlar');
+var UrinishlarSoniO = 6;
+var arrrey = [];
+
+elForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+
+  // imkoniyatlar soni
+
+  elUrinishlarSoni.textContent = --UrinishlarSoniO;
+  if(UrinishlarSoniO === 0) {
+    elInput.remove();
+    elBtn.remove();
+    elOutput.textContent = 'Qayta Urinib Koring...';
+    return;
+  }
+
+
+
+  var elUserNumber = Number(elInput.value);
+  elUrinishlarOutputArray.classList.add('h4');
+
+  // array
+  arrrey.push(elUserNumber);
+  elUrinishlarOutputArray.textContent = arrrey.join(', ');
+
+  elOutput.classList.add('h4', 'text-danger');
+
+
+  if(elUserNumber > compNumber) {
+    elOutput.textContent = "Kichikroq son kiriting";
+  }else if (elUserNumber < compNumber) {
+    elOutput.textContent = "Kottaro son kiriting";
+  }else if ( elUserNumber === compNumber) {
+    elOutput.textContent = "Topdingiz";
+  }
+});
